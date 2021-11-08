71e2a62da2b9f4e6b8882a6fdc2a0791a07f0930
LS-hash <93827347+LS-hash@users.noreply.github.com>
2021-11-08 19:11:32 +0100

    added more rows  to the forecast and made the js work

--- src/index.js
+++ src/index.js
@@ -83,21 +83,32 @@ navigator.geolocation.getCurrentPosition(retrievePosition);
 
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = `
-    <div class="row">
-            <div class="col-2">
-              <div class="wather-forecast-date">Fri</div>
-              <img
-                src="http://openweathermap.org/img/wn/04d@2x.png"
-                width="36px"
-              />
-              <div class="weather-forecast-temperatures">
-                <span class="weather-forecast-temperatures-max"> 18 </span>
-                <span class="weather-forecast-temperatures-min"> 12 </span>
-              </div>
-            </div>
-    </div>
-    `;
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
 }
 
-displayForecast;
+displayForecast();
